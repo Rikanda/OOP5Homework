cadc671d6278052ac4dde3e93f0a8f9b2d05be98
Natalya <soltk7@yandex.ru>
2021-11-06 06:20:04 +0300

    Add project files.

---
+++ OOP5Homework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP5Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
